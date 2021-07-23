fda0171a138d0cbcac50f491ebc190d89671d4d4
wyqjolin <1983371685@qq.com>
2021-07-23 19:09:47 +0800

    task0

---
+++ ps0.py
@@ -0,0 +1,9 @@
+import math
+
+x = int(input("Please enter a number: "))
+y = int(input("Please enter another number: "))
+print("Enter number x: ",x)
+print("Enter number y: ",y)
+ans = x**y
+print("X**y =",ans)
+print("log(x) =",int(math.log2(x)))
